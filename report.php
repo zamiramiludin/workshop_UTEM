8102ec854a43b6b10005f6dccb18b2a12ab902eb
zamiramiludin <zamiramiludin@gmail.com>
2018-04-22 17:08:58 +0800

    first commit

--- report.php
+++ report.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="ms">
 <head>
@@ -37,7 +38,9 @@
                     </div>
                     </div>
                 </nav>
-    
+                
+        <?php if (isset($_SESSION['status'] == logged )) { ?> 
+            <!-- else masuk modal -->
 		
         <div class="container bg-light"> 
 						<br>
@@ -130,26 +133,16 @@
         </form>
         </div>
         </div>
-    </div>
-    <button id="la">LOL</button>
-    <script>
-        $(document).ready(function(){
-        $('#la').click(function(){
-            swal({
-                title: "confirmation",
-                text: "Aku dah pening!!!",
-                buttons: {
-                    cancel: true,
-                    confirm: "Submit"
-                }
-            })
-        })
-    });
-    </script>
-        
+    </div>    
+
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	  <script>
+        $('#submit-button').mousedown(function(){
+            swal("Good job!", "You clicked the button!", "info");
+        });
+    </script>
 </body>
 </html>
